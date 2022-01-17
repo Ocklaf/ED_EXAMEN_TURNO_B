a7c1e8c95102168d258926645577c0b60c20662c
José Vicente Falcó <ocklaf@gmail.com>
2022-01-17 15:56:01 +0100

    versión final con javadoc DNI 33563120W

--- AnalistaED_33563120W/src/AnalistaED_33563120W.java
+++ AnalistaED_33563120W/src/AnalistaED_33563120W.java
@@ -1,17 +1,32 @@
 
 import java.util.*;
-
+/**
+ * <h1>EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED</h1>
+ * @author JOSÉ VICENTE FALCÓ 33563120W
+ * @version 1.0
+ */
 public class AnalistaED_33563120W {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    /**
+     * Constructor con tres parámetros
+     * @param nombreCompleto Nombre completo en mayúsculas
+     * @param dni DNI Documento Nacional de Identidad con letra y sin espacios
+     */
     public AnalistaED_33563120W(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
 
+    /**
+     * Método getter que Calcula el salario bruto mensual en fucnión del salario base por hora y las horas extras.
+     * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+     * @param horasExtra Número de horas extras dedicadas redondeado a la baja (entero)
+     * @return Devuelve el salario bruto mensual en euros con dos decimales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora
