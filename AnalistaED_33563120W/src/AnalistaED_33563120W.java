ee32de3246ace16eb87e27ee77c6e015b8e0f9e2
José Vicente Falcó <ocklaf@gmail.com>
2022-01-17 15:49:13 +0100

    Código sin javadoc DNI 33563120W

---
+++ AnalistaED_33563120W/src/AnalistaED_33563120W.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class AnalistaED_33563120W {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_33563120W(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora
+                * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        AnalistaED_33563120W emp = new AnalistaED_33563120W("JOSÉ VICENTE FALCÓ",
+                "33563120W");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+}
